82c010fa26d0fd6319e3a7e70f5cfa13d415b885
nkbadhon <badhon7574@gmail.com>
2023-01-07 13:40:28 +0600

    Navbar updated with light theme

--- src/Pages/Home/Home/Shared/Navbar/Navbar.js
+++ src/Pages/Home/Home/Shared/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function Navbar() {
-    // Declare a state variable for the dark mode setting
+    // Declaring a state variable for the dark mode setting
     const [darkMode, setDarkMode] = useState(false)
 
     // Toggle the dark mode setting when the button is clicked
@@ -9,12 +9,38 @@ function Navbar() {
         setDarkMode(!darkMode)
     }
     return (
-        <nav className={darkMode ? 'dark-mode' : 'light-mode'}>
-            <h1>Annotator</h1>
-            <button onClick={toggleDarkMode}>
-                {darkMode ? 'Light Mode' : 'Dark Mode'}
-            </button>
-        </nav>
+
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li tabIndex={0}>
+                        <a>
+                            Parent
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                        </a>
+                        <ul className="p-2 bg-base-100">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+
+
+        // <nav className={darkMode ? 'dark-mode' : 'light-mode'}>
+        //     <h1>Annotator</h1>
+        //     <button onClick={toggleDarkMode}>
+        //         {darkMode ? 'Light Mode' : 'Dark Mode'}
+        //     </button>
+        // </nav>
     );
 };
 
