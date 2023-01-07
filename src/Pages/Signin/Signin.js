fd9a559297c6281c64d348359f371a017d6d4d5e
nkbadhon <badhon7574@gmail.com>
2023-01-07 19:48:44 +0600

    Almost done login page

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 
 const Signin = () => {
 
-    const { register, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
     const handleSignin = data => {
         console.log(data);
     }
@@ -21,12 +21,14 @@ const Signin = () => {
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Email</span>
                         </label>
-                        <input {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input {...register("email", { required: "Email address is required" })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600' role="alert">{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Password</span>
                         </label>
-                        <input {...register("pass")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password" {...register("pass", { required: "Password is required" })} className="input input-bordered w-full max-w-xs" />
+                        {errors.pass && <p className='text-red-600' role="alert">{errors.pass?.message}</p>}
                     </div>
                     <input className='btn w-full' value="Sign In" type="submit" />
                 </form>
