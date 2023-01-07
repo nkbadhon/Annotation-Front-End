a1586098ef28aa2ed5420460df10740f4382c477
nkbadhon <badhon7574@gmail.com>
2023-01-07 16:09:35 +0600

    Working on login page

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+
+
 
 const Signin = () => {
+
+    const { register, handleSubmit } = useForm()
+    const [data, setData] = useState("")
+
+
     return (
-        <div>
-            Sign In Page.
+        <div className='h-[800px] flex justify-center items-center'>
+            <div>
+                <h2 className='text-4xl'> SignIn</h2>
+
+                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("email")} className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("pass")} className="input input-bordered w-full max-w-xs" />
+                    </div>
+
+
+                    <p>{data}</p>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
